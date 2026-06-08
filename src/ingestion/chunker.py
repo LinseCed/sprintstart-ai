081e69f52815b0578c225557412a1237a1bf6efa
DaniloTatti <danilo.10102003@outlook.com>
2026-06-08 09:42:56 +0200

    updated chunker with requested changes

--- src/ingestion/chunker.py
+++ src/ingestion/chunker.py
@@ -71,8 +71,7 @@ def chunk_code(filename: str, code: str, chunk_size: int = 512) -> list[ParsedCh
             Sequential code chunks with metadata and chunk indices.
     """
     lines: list[str] = code.splitlines()
-    chunk_count: int = 0
-    chunks_content: dict[int, str] = {}
+    chunks_content: list[str] = []
     current_chunk_content: list[str] = []
     current_chunk_content_length = 0
     for line in lines:
@@ -80,8 +79,7 @@ def chunk_code(filename: str, code: str, chunk_size: int = 512) -> list[ParsedCh
             current_chunk_content_length + len(line) > chunk_size
             and current_chunk_content
         ):
-            chunks_content[chunk_count] = "\n".join(current_chunk_content)
-            chunk_count += 1
+            chunks_content.append("\n".join(current_chunk_content))
             current_chunk_content = []
             current_chunk_content_length = 0
 
@@ -90,7 +88,9 @@ def chunk_code(filename: str, code: str, chunk_size: int = 512) -> list[ParsedCh
         # brings a line break (/n) with it
 
     if current_chunk_content:
-        chunks_content[chunk_count] = "\n".join(current_chunk_content)
+        chunks_content.append("\n".join(current_chunk_content))
+
+    total_chunks_amount: int = len(chunks_content)
 
     return [
         ParsedChunk(
@@ -99,8 +99,8 @@ def chunk_code(filename: str, code: str, chunk_size: int = 512) -> list[ParsedCh
             metadata={
                 **build_metadata(Path(filename)),
                 "chunk_index": str(chunk_index),
-                "total_chunks": str(chunk_count),
+                "total_chunks": str(total_chunks_amount),
             },
         )
-        for chunk_index, chunk_content in chunks_content.items()
+        for chunk_index, chunk_content in enumerate(chunks_content)
     ]
